804cb81e33774a605cd2b615281e321697d33cfe
RoyerYL <royeryl03@gmail.com>
2026-05-19 13:20:20 -0300

    mover jugadores

--- Desafio 3/desafio3.py
+++ Desafio 3/desafio3.py
@@ -16,6 +16,8 @@ def generar_cancha():
 def registrar_jugadores(jugadores,cancha):
     for jugador in jugadores:
         cancha[int(jugador["fila"])][int(jugador["columna"])]=jugador["equipo"]
+    
+    return True
 
 def subir_jugadores(archivo):
 
@@ -29,8 +31,8 @@ def subir_jugadores(archivo):
                     {
                         "nombre":jugador[0].strip(),
                         "equipo":jugador[1].strip(),
-                        "fila":jugador[2].strip(),
-                        "columna":jugador[3].strip(),
+                        "fila":int(jugador[2].strip()),
+                        "columna":int(jugador[3].strip()),
                         "rol":jugador[4].strip(),
                         "tiene_pelota":jugador[5].strip()
                         
@@ -40,11 +42,56 @@ def subir_jugadores(archivo):
         raise FileNotFoundError("El archivo no existe")
 
     return jugadores
+def movimientos():
+    movimientos_posibles=["arriba","abajo","izquierda","derecha"]
+    print("1 - Arriba")
+    print("2 - Abajo")
+    print("3 - Izquierda")
+    print("4 - Derecha")
+    posicion=int(input("Seleccion una opción:"))
+    return movimientos_posibles[posicion-11]
+
+def mover(index,jugadores,cancha):
+
+    jugador=jugadores[index]
+
+    fila=jugador["fila"]
+    columna=jugador["columna"]
+
+    movimiento=movimientos()
+    
+    match movimiento:
+        case "arriba":
+            cancha[fila-1][columna]=jugador["equipo"]
+        case "abajo":
+            cancha[fila+1][columna]=jugador["equipo"]
+        case "izquierda":
+            cancha[fila][columna-1]=jugador["equipo"]
+        case "derecha":
+            cancha[fila][columna+1]=jugador["equipo"]
+        case _:
+            print("Opcion no valida")
+    cancha[fila][columna]="."
+
+def mover_jugadores(jugadores,cancha):
+
+    for index,jugador in enumerate(jugadores):
+        print(f"{index } - {jugador["nombre"]}")
+    jugador_index=int(input("Seleccione un jugador para mover: "))
+    
+    mover(jugador_index,jugadores,cancha)
+    
+    return True
 
 def menu(cancha):
     continuar=True
+    jugadores_registrados=False
+    jugadores=[]
+    
+    mostrar_partido(cancha)
+
     while continuar:
-        if jugadores:
+        if not jugadores_registrados:
             print("1 - Registrar jugadores")
         print("2 - Mover jugadores en cancha")
         print("3 - Calcular distancia")
@@ -55,8 +102,9 @@ def menu(cancha):
         match opcion:
             case 1:
                 jugadores=subir_jugadores("archivo.txt")
+                jugadores_registrados = registrar_jugadores(jugadores,cancha)
             case 2:
-                print("1")
+                mover_jugadores(jugadores,cancha)
             case 3:
                 print("1")
             case 4:
@@ -65,8 +113,10 @@ def menu(cancha):
                 print("1")
             case _:
                 print("Opcion no valida")
-            
-        registrar_jugadores(jugadores,cancha)
+
+        mostrar_partido(cancha)
+    
+    
     
 
 def mostrar_partido(cancha):
@@ -81,7 +131,6 @@ def main():
     
     menu(cancha)
     
-    mostrar_partido(cancha)
     
 
 if __name__ == "__main__":
