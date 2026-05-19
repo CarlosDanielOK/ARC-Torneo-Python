67bde55a0ac14de394f0c883cfca231447215459
RoyerYL <royeryl03@gmail.com>
2026-05-19 10:52:11 -0300

    opciones subir y generar jugadores en cancha

--- Desafio 3/desafio3.py
+++ Desafio 3/desafio3.py
@@ -15,7 +15,7 @@ def generar_cancha():
 
 def registrar_jugadores(jugadores,cancha):
     for jugador in jugadores:
-        print(jugador)
+        cancha[int(jugador["fila"])][int(jugador["columna"])]=jugador["equipo"]
 
 def subir_jugadores(archivo):
 
@@ -24,31 +24,65 @@ def subir_jugadores(archivo):
     try:
         with open(archivo, "r") as jugadores_lista:
             for linea in jugadores_lista:
-                print(linea)
-    
+                jugador=linea.split("|")
+                jugadores.append(
+                    {
+                        "nombre":jugador[0].strip(),
+                        "equipo":jugador[1].strip(),
+                        "fila":jugador[2].strip(),
+                        "columna":jugador[3].strip(),
+                        "rol":jugador[4].strip(),
+                        "tiene_pelota":jugador[5].strip()
+                        
+                        })
+
     except FileNotFoundError:
         raise FileNotFoundError("El archivo no existe")
 
     return jugadores
 
 def menu(cancha):
-    print("1 - Registrar jugadores")
-    print("2 - Mover jugadores en cancha")
-    print("3 - calcular distancia")
-    print("4 - detectar posibles pases")
-    print("5 - detectar posibles ofensivas")
-    opcion=int(input("Seleccione una opcion: "))
-    return opcion
+    continuar=True
+    while continuar:
+        if jugadores:
+            print("1 - Registrar jugadores")
+        print("2 - Mover jugadores en cancha")
+        print("3 - Calcular distancia")
+        print("4 - Detectar posibles pases")
+        print("5 - Detectar posibles ofensivas")
+        opcion=int(input("Seleccione una opcion: "))
+
+        match opcion:
+            case 1:
+                jugadores=subir_jugadores("archivo.txt")
+            case 2:
+                print("1")
+            case 3:
+                print("1")
+            case 4:
+                print("1")
+            case 5:
+                print("1")
+            case _:
+                print("Opcion no valida")
+            
+        registrar_jugadores(jugadores,cancha)
+    
 
 def mostrar_partido(cancha):
         for fila in cancha:
             print("".join(fila))  
 
 def main():
+
     cancha = generar_cancha()
+    
+    mostrar_partido(cancha)
+    
     menu(cancha)
-    subir_jugadores("archivo.txt")
-    # mostrar_partido(cancha)
+    
+    mostrar_partido(cancha)
+    
 
 if __name__ == "__main__":
     main()
